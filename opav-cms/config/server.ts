de28426f8b22ae5c5a64806c06910f9819ad60a1
Camilo Peñuela <caenpes2003@gmail.com>
2026-03-15 22:03:34 -0500

    fix: cms

--- opav-cms/config/server.ts
+++ opav-cms/config/server.ts
@@ -2,7 +2,9 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL'),
-  proxy: true,
+  proxy: {
+    koa: true,
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
